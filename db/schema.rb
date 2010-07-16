50a5fad4579c50b1ebaa54d495f341324bd1894a
Brian Sullivan <bmsullivan@gmail.com>
2010-07-15 21:25:07 -0500

    Added projects model

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100715035318) do
+ActiveRecord::Schema.define(:version => 20100716013243) do
+
+  create_table "projects", :force => true do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
